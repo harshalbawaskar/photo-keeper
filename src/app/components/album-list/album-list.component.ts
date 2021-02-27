ed15ef2ef1715293181aa58f479a41a6a3e046c4
harshalbawaskar <harshalbawaskar>
2021-02-27 21:57:41 +0530

    add show photo button

--- src/app/components/album-list/album-list.component.ts
+++ src/app/components/album-list/album-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { PhotoAlbumService } from 'src/app/services/photo-album.service';
 import { Album } from 'src/app/types/album';
+import { User } from 'src/app/types/user';
 
 @Component({
   selector: 'app-album-list',
@@ -12,11 +13,26 @@ export class AlbumListComponent implements OnInit {
 
   albumList: Album[];
 
+  selectedAlbums: Album[] = [];
+
   constructor(private photoAlbumService: PhotoAlbumService) { }
 
   ngOnInit(): void {
     this.photoAlbumService.getAlbums(1).subscribe((albums: Album[]) => {
       this.albumList = albums;
     });
+  }
+
+  onAlbumSelect(album: Album) {
+    const foundAlbum = this.selectedAlbums.find(abm => abm.id === album.id);
+    if (foundAlbum) {
+      this.selectedAlbums = this.selectedAlbums.filter(abm => abm.id !== album.id);
+    } else {
+      this.selectedAlbums.push(album);
+    }
+  }
+
+  showPhotos() {
+    // TODO: Navigate user to photos.
   }
 }
