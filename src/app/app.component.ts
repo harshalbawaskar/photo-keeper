af2de5e3376bf18f29c0d03877cafb028a931664
harshalbawaskar <harshalbawaskar>
2021-02-28 19:53:18 +0530

    add back button in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StateDeclaration, StateService, Transition, TransitionService, UIRouterGlobals } from '@uirouter/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'photo-keeper';
+  isUsersView: boolean;
+
+  constructor(private stateService: StateService,
+    private uiRouterGlobals: UIRouterGlobals,
+    private transition: TransitionService) {}
+
+  ngOnInit(): void {
+    this.transition.onSuccess(null, (transition: Transition) => {
+      this.isUsersView = this.uiRouterGlobals.current.name === 'users';
+    })
+  }
+
+  onBackClick() {
+    const state = this.getPreviousState();
+    this.stateService.go(state.name, state.pathParam);
+  }
+
+  getPreviousState() {
+    switch (this.uiRouterGlobals.current.name) {
+      case 'albums':
+        return {
+          name: 'users',
+          pathParam: {
+            userId: this.uiRouterGlobals.params.userId
+          }
+        };
+      case 'photos':
+      return {
+        name: 'albums',
+        pathParam: {
+          userId: this.uiRouterGlobals.params.userId,
+          albumIds: this.uiRouterGlobals.params.albumIds
+        }
+      };
+    }
+  }
 }
